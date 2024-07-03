ed80cca1b760056a19df69a070364a40e50bdadb
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2024-07-03 14:01:15 +0530

    -sm

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,6 +4,7 @@ const Card = ({ icon , text  }) => {
     return (
         <div className=' bg-yellow-500 w-max p-4 m-4'>
             <div>{icon}</div>
+            
             <div>{text}</div>
         </div>
     );
